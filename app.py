54a835c0e6c08f93711a4993a1a5d02bbcd524a4
Piyush Upadhyay <piyushupadhyay2566@gmail.com>
2023-06-25 08:08:53 -0400

    skeleton endpoints added

--- app.py
+++ app.py
@@ -3,6 +3,31 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("parties/host")
+def host_party():
+    pass
+
+
+@app.route("parties")
+def get_recommended_parties():
+    pass
+
+
+@app.route("parties/<party_id>")
+def get_party_details():
+    pass
+
+
+@app.route("parties/<party_id>/attend")
+def attend_party(party_id):
+    pass
+
+
+@app.route("user/parties/attend")
+def get_user_attend_parties():
+    pass
+
+
+@app.route("user/parties/host")
+def get_user_host_parties():
+    pass
