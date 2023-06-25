f9625c246ca9eca64fcad85ce86b326f0c41055b
Preeti <preetivalunjkar1540@gmail.com>
2023-06-25 18:21:25 -0400

    cherry picked preeti's changes from preeti/HostPage

--- Vibees/app/src/main/java/com/example/vibees/screens/auth/LoginScreen.kt
+++ Vibees/app/src/main/java/com/example/vibees/screens/auth/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.vibees.screens.auth
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,9 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
 
 @Composable
 fun LoginScreen(
@@ -18,15 +23,15 @@ fun LoginScreen(
     onForgotClick: () -> Unit
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().background(Color.Yellow),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             modifier = Modifier.clickable { onClick() },
-            text = "LOGIN",
-            fontSize = MaterialTheme.typography.headlineMedium.fontSize,
-            fontWeight = FontWeight.Bold
+            text = "VIBEES",
+            fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+            fontWeight = FontWeight.Bold,
         )
         Text(
             modifier = Modifier.clickable { onSignUpClick() },
