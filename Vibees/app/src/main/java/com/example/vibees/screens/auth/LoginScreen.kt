f592846cf66d70b89cba6ce755f6c7e12de06b9b
Piyush Upadhyay <piyushupadhyay2566@gmail.com>
2023-06-26 00:40:03 -0400

    create a global state for access

--- Vibees/app/src/main/java/com/example/vibees/screens/auth/LoginScreen.kt
+++ Vibees/app/src/main/java/com/example/vibees/screens/auth/LoginScreen.kt
@@ -4,7 +4,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +19,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import com.example.vibees.GlobalAppState
+import com.example.vibees.graphs.PartyScreen
 
 @Composable
 fun LoginScreen(
@@ -22,17 +31,49 @@ fun LoginScreen(
     onSignUpClick: () -> Unit,
     onForgotClick: () -> Unit
 ) {
+    var userID by GlobalAppState::UserID
+    var userName by GlobalAppState::UserName
     Column(
-        modifier = Modifier.fillMaxSize().background(Color.Yellow),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Yellow),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            modifier = Modifier.clickable { onClick() },
-            text = "VIBEES",
-            fontSize = MaterialTheme.typography.headlineLarge.fontSize,
-            fontWeight = FontWeight.Bold,
-        )
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+            horizontalArrangement = Arrangement.Center) {
+            androidx.compose.material.Button(
+                onClick = {
+                    userID = "6515c9f8-57f1-406f-8707-20033dcd764e"
+                    userName = "Christian"
+                    onClick()
+                },
+                modifier = Modifier.padding(20.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colorScheme.primary)
+            ) {
+                androidx.compose.material.Text(
+                    text = "Christian User",
+                    color = Color.Black,
+                )
+            }
+
+            androidx.compose.material.Button(
+                onClick = {
+                    userID = "5bdfc21f-ea15-43b3-9654-093f15d63ba7"
+                    userName = "Shawn"
+                    onClick()
+                },
+                modifier = Modifier.padding(20.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colorScheme.primary)
+            ) {
+                androidx.compose.material.Text(
+                    text = "Shawn User",
+                    color = Color.Black
+                )
+            }
+        }
         Text(
             modifier = Modifier.clickable { onSignUpClick() },
             text = "Sign Up",
