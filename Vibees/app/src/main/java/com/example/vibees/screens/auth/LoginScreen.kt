1a2c5b40f5968e8108314b65751ac1ef684c8d65
Preeti <preetivalunjkar1540@gmail.com>
2023-07-03 15:46:33 -0400

    Changes to login page UI and color theme

--- Vibees/app/src/main/java/com/example/vibees/screens/auth/LoginScreen.kt
+++ Vibees/app/src/main/java/com/example/vibees/screens/auth/LoginScreen.kt
@@ -5,25 +5,101 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import com.example.vibees.GlobalAppState
 import com.example.vibees.graphs.PartyScreen
+
+
+@Composable
+fun LoginTextField(
+    modifier: Modifier = Modifier,
+    text: String,
+    placeholder: String,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    onChange: (String) -> Unit = {},
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    keyBoardActions: KeyboardActions = KeyboardActions(),
+    isEnabled: Boolean = true
+) {
+    OutlinedTextField(
+        modifier = modifier
+            .fillMaxWidth(0.90f)
+            .padding(10.dp)
+            .background(Color.White),
+        value = text,
+        onValueChange = onChange,
+        leadingIcon = leadingIcon,
+        textStyle = TextStyle(fontSize = 18.sp),
+        keyboardOptions = KeyboardOptions(imeAction = imeAction, keyboardType = keyboardType),
+        keyboardActions = keyBoardActions,
+        enabled = isEnabled,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Black,
+            unfocusedBorderColor = Color.Gray,
+            disabledBorderColor = Color.Gray,
+            disabledTextColor = Color.Black
+        ),
+        placeholder = {
+            androidx.compose.material.Text(text = placeholder, style = TextStyle(fontSize = 18.sp, color = Color.DarkGray))
+        }
+    )
+}
+
+
+@Composable
+fun AppName() {
+    Text( text =
+    buildAnnotatedString {
+        withStyle(style = SpanStyle(color = Color.White)) {
+            append("VI")
+        }
+        withStyle(style = SpanStyle(color = MaterialTheme.colorScheme.primary)) {
+            append("BEE")
+        }
+        withStyle(style = SpanStyle(color = Color.White)) {
+            append("S")
+        }
+    },
+        fontSize = 70.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
 
 @Composable
 fun LoginScreen(
@@ -33,13 +109,100 @@ fun LoginScreen(
 ) {
     var userID by GlobalAppState::UserID
     var userName by GlobalAppState::UserName
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Yellow),
+            .background(Color.Black),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+//        Text(text = "VIBEES", color = Color.White, fontWeight = FontWeight.Bold,
+//             fontSize = MaterialTheme.typography.headlineLarge.fontSize)
+
+        AppName()
+
+        Spacer(modifier = Modifier.padding(12.dp))
+
+        LoginTextField(
+            text = username,
+            placeholder = "Username*",
+            onChange = {
+                username = it
+            },
+            imeAction = ImeAction.Next,//Show next as IME button
+            keyboardType = KeyboardType.Text, //Plain text keyboard
+            keyBoardActions = KeyboardActions(
+                onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                }
+            )
+        )
+
+        LoginTextField(
+            text = password,
+            placeholder = "Password*",
+            onChange = {
+                password = it
+            },
+            imeAction = ImeAction.Next,//Show next as IME button
+            keyboardType = KeyboardType.Text, //Plain text keyboard
+            keyBoardActions = KeyboardActions(
+                onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                }
+            )
+        )
+
+        Row (modifier = Modifier
+            .fillMaxWidth(0.90f)
+            .padding(10.dp),
+             horizontalArrangement = Arrangement.End) {
+            Text(
+                modifier = Modifier.clickable { onForgotClick() },
+                text = "Forgot Password?",
+                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                fontWeight = FontWeight.Medium,
+                color = Color.White
+            )
+        }
+
+        androidx.compose.material.Button(
+            onClick = { onSignUpClick() },
+            modifier = Modifier,
+            colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colorScheme.primary)
+        ) {
+            androidx.compose.material.Text(
+                text = "Sign In",
+                color = Color.Black,
+                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Text (text = "OR",
+              color = MaterialTheme.colorScheme.primary,
+              fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+              fontWeight = FontWeight.Bold)
+
+        androidx.compose.material.Button(
+            onClick = { onSignUpClick() },
+            modifier = Modifier,
+            colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colorScheme.primary)
+        ) {
+            androidx.compose.material.Text(
+                text = "Create an account",
+                color = Color.Black,
+                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+
         Row(modifier = Modifier
             .fillMaxWidth()
             .padding(10.dp),
@@ -74,17 +237,5 @@ fun LoginScreen(
                 )
             }
         }
-        Text(
-            modifier = Modifier.clickable { onSignUpClick() },
-            text = "Sign Up",
-            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-            fontWeight = FontWeight.Medium
-        )
-        Text(
-            modifier = Modifier.clickable { onForgotClick() },
-            text = "Forgot Password",
-            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-            fontWeight = FontWeight.Medium
-        )
     }
 }
