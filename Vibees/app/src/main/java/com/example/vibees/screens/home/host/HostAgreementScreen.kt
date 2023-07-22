8a55f852ce7b016f840307d575d3d77c20064cb4
Preeti <preetivalunjkar1540@gmail.com>
2023-07-22 17:19:13 -0400

    Added bullet points for guidelines

--- Vibees/app/src/main/java/com/example/vibees/screens/home/host/HostAgreementScreen.kt
+++ Vibees/app/src/main/java/com/example/vibees/screens/home/host/HostAgreementScreen.kt
@@ -1,22 +1,16 @@
 package com.example.vibees.screens.home.host
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CheckboxColors
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,10 +19,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import me.naingaungluu.formconductor.FormResult
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HostAgreementScreen(onClick: () -> Unit) {
@@ -44,31 +41,52 @@ fun HostAgreementScreen(onClick: () -> Unit) {
         Text(
             "Want to host a party?",
             modifier = Modifier.padding(10.dp),
-            fontWeight = FontWeight.SemiBold,
+            fontWeight = FontWeight.Bold,
             fontSize = MaterialTheme.typography.titleLarge.fontSize,
             color = Color.Black
         )
 
         Text(
-            "Agree to the guidelines and be set to host!",
+            "Accept the guidelines and get ready to rock!",
             fontWeight = FontWeight.SemiBold,
             fontSize = MaterialTheme.typography.bodyMedium.fontSize,
             color = Color.Gray
         )
 
         Text(
-            "For the safety and well-being of the community around you, please ensure that " +
-                    "you keep noise pollution to a minimum, especially during the hours of 10pm to" +
-                    "7am. If there are substances or harmful supplies which may be used in the party," +
-                    " inform your neighbours and attendees prior to your party. Prioritize their as well as your welfare and " +
-                    "enjoy your party safely!",
-            modifier = Modifier.padding(20.dp),
+            "As a host, please agree to the following guidelines to ensure the safety and well-being of " +
+                    "the community during your parties and events:",
+            modifier = Modifier.padding(5.dp, 20.dp, 5.dp, 15.dp),
             fontWeight = FontWeight.Normal,
             fontSize = MaterialTheme.typography.titleSmall.fontSize,
             color = Color.Black
         )
+        val bullet = "\u2022"
+        val messages = listOf(
+            "Control noise levels during quiet hours between 10pm and 7am to be considerate of neighbors",
+            "Obtain necessary permits for large-scale events and adhere to safety measures for the welfare of attendees",
+            "Disclose any potentially harmful party supplies to guests and nearby residents, using them responsibly and disposing of them properly",
+            "Implement strict safety protocols if your event involves hazardous materials or pyrotechnics to prevent accidents",
+            "Ensure compliance with age restrictions for alcohol consumption to maintain a safe environment and prevent underage drinking"
+        )
+        val paragraphStyle = ParagraphStyle(textIndent = TextIndent(restLine = 12.sp))
+        Text(
+            buildAnnotatedString {
+                messages.forEach {
+                    withStyle(style = paragraphStyle) {
+                        append(bullet)
+                        append("\t\t")
+                        append(it)
+                    }
+                }
+            },
+            modifier = Modifier.padding(5.dp, 0.dp, 5.dp, 10.dp),
+            fontWeight = FontWeight.Light,
+            fontSize = MaterialTheme.typography.titleSmall.fontSize,
+            color = Color.Black
+        )
 
-        Row(modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 20.dp)) {
+        Row(modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 10.dp)) {
             Checkbox(checked = checked,
                 onCheckedChange = { checked_ -> checked = checked_ },
                 colors = CheckboxDefaults.colors(
